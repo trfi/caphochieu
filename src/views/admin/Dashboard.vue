eebbfd8c474fbf37fa4dfd326527448f3d523d14
tr-fi <gnourtf@gmail.com>
2020-04-09 14:06:57 +0700

    fix eslint

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -49,22 +49,22 @@ export default {
 </script>
 
 <style>
-.passport.complete{
-  border-left: 4px solid #3cd1c2;
-}
-.passport.waiting{
-  border-left: 4px solid #ffaa2c;
-}
-.passport.cancel{
-  border-left: 4px solid #f83e70;
-}
-div.right > .v-chip.complete{
-  background: #3cd1c2;
-}
-div.right > .v-chip.waiting{
-  background: #ffaa2c;
-}
-div.right > .v-chip.cancel{
-  background: #f83e70;
-}
+  .passport.complete{
+    border-left: 4px solid #3cd1c2;
+  }
+  .passport.waiting{
+    border-left: 4px solid #ffaa2c;
+  }
+  .passport.cancel{
+    border-left: 4px solid #f83e70;
+  }
+  div.right > .v-chip.complete{
+    background: #3cd1c2;
+  }
+  div.right > .v-chip.waiting{
+    background: #ffaa2c;
+  }
+  div.right > .v-chip.cancel{
+    background: #f83e70;
+  }
 </style>
